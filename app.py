a495570d3a0898e5492b2b6629a2790014d27f06
Dominik Boehler <dominik.boehler@gmx.net>
2023-01-09 15:44:27 +0100

    added json end env data

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+
+from flask import Flask
+import json
+
+
+with open("PatientData.json") as f:
+    data = json.load(f)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return str(data)
+
